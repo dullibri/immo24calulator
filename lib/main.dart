4bb79b783ee30a05ce487f4baca339d7003bb181
Dirk Ulbricht <dirk.ulbricht@gmail.com>
2024-08-30 15:09:39 +0200

    Added Provider package and implemented MultiProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'home_page.dart';
+import 'mortgage_calculator_provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MortgageCalculatorProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
